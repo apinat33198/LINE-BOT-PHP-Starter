90d879bfa3148cd2a31a129790b8873f2806854e
Apinat Poungnil <5622793123@studentmail.siit.tu.ac.th>
2017-01-26 21:59:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,10 +17,20 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			$text_ex = explode('!', $text);
+			if($text_ex[0]== "cal"){
+				$res = $text_ex[1];
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $res
 			];
+			}
+			else {
+				$message = [
+					'type' => 'text',
+					'text' => 'Wrong command mofo'
+			];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
